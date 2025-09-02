b2ebc7acd2030df43d3023b056d46f26e68062af
Deepakkumar054 <154993427+Deepakkumar054@users.noreply.github.com>
2025-09-02 12:47:26 +0530

    Update CORS and frontend env for Render deployment

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
     "http://localhost:5173",           // local frontend
-    "https://fooddel-ojd3.onrender.com" // deployed frontend
+    "https://fooddel-frontend-dezf.onrender.com" // deployed frontend
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
